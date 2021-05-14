8d6a333625455963ab32fff3e59fcec42cd67fe2
sara <sara.sabinm@gmail.com>
2021-05-14 13:42:20 +0200

    exo

---
+++ js/homepage.js
@@ -0,0 +1,5 @@
+
+//const URL_API_KEY = "b8e16ff25f44004fe2ab5dedc9e0453e";
+//const URL_API_MOVIE_SEARCH = "https://api.themoviedb.org/3/search/movie/";
+//const URL_IMAGE_PREFIX = "https://image.tmdb.org/t/p/w200";
+
